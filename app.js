873b03cffc4491f4d6ab9f390203aa78b83c179f
syed-minhaj-hussain <syed-minhaj-hussain@users.noreply.github.com>
2021-04-17 19:15:03 +0530

    added responsiveness

---
+++ app.js
@@ -0,0 +1,7 @@
+(function getResolution() {
+    let resolution = `${window.screen.width} x ${window.screen.height}`;
+    let width = window.screen.width;
+    if(width <= 770){
+        alert(`want to view the documentation code? Then Switch On the Desktop mode`)
+    }
+})();
